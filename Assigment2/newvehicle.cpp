36a4e04f47a3f3198bdeb91cf8fcd41901bc6fcb
AleXs-OX <55241488+AleXs-OX@users.noreply.github.com>
2021-03-09 12:46:09 +0100

    Added link class

---
+++ Assigment2/newvehicle.cpp
@@ -0,0 +1,20 @@
+ #include "newvehicle.h"
+
+
+/*EN ESTA CLASE IRAN LOS IFS y LOS METODOS ENCARGADOS DE LA ADMINISTRACION DE LA CLASE VEHICLE*/
+
+/*Constructor vacio (de momento)*/
+newVehicle::newVehicle()
+{
+
+}
+
+void addVehicle(){
+
+     /*Creacion de nuevo vehiculo*/
+    /*La idea es que desde mainWindow.cpp se llame a esta clase, que sera el enlace entre intefaz & vehicle.cpp
+    que sera la clase que crea el vehiculo como tal*/
+
+    //vehicles.push_back(Vehicle vehicle("Informacion"));
+
+}
---
+++ Assigment2/newvehicle.h
@@ -0,0 +1,11 @@
+#ifndef NEWVEHICLE_H
+#define NEWVEHICLE_H
+
+
+class newVehicle
+{
+public:
+    newVehicle();
+};
+
+#endif // NEWVEHICLE_H
