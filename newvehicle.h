510b627c974d03fc5ed6a02bfed910ad8b7715e8
jsalig00 <36676028+Jsalig00@users.noreply.github.com>
2021-03-14 15:29:17 +0100

    Update newvehicle.h

--- newvehicle.h
+++ newvehicle.h
@@ -1,21 +1,51 @@
-#ifndef NEWVEHICLE_H
-#define NEWVEHICLE_H
-#include <iostream>
-#include "vehicle.h"
-
-class newVehicle
+#ifndef VEHICLE_H
+#define VEHICLE_H
+#include "iostream"
+#include <string>
+class Vehicle
 {
 public:
+    Vehicle();
+    Vehicle(std::string name,int nWheels,bool engine,int engineCV,bool fuel,char fuelType,std::string color,bool wings,bool reactor,bool undercarriage,bool locomotive,bool wagon,int numWagon,bool spareWheel,std::string registration);
+ 
+    /*vehicle::name = name;
+    vehicle::nWheels = nWheels;
+    vehicle::engine = engine;
+    vehicle::engineCV = engineCV;
+    vehicle::fuel = fuel;
+    vehicle::fuelType = fuelType;
+    vehicle::color = color;
+    vehicle::wings = wings;
+    vehicle::reactor = reactor;
+    vehicle::undercarriage = undercarriage;
+    vehicle::locomotive = locomotive;
+    vehicle::wagon = wagon;
+    vehicle:: spareWheel = spareWheel;
+    vehicle::registration = registration;*/    
+    
+    //Metodos Getter
+    std::string getName();
+    std::string getType();
+    std::string getNWheels();
+    std::string getEngine();
+    std::string getEngineCV();
+    std::string getFuel();
+    std::string getFuelType();
+    std::string getColor();
+    bool getWings();
+    bool getReactor();
+    std::string getUndercarriage();
+    std::string getLocomotive();
+    std::string getWagon();
+    std::string getSpareWheel();
+    std::string getRegistration();    
 
-    newVehicle();
-    void setUndercarriage(bool undercarriage);
-    void setLocomotive(bool locomotive);
-    void setWagon(bool wagon);
-    void setNumWagon(int numWagon);
-    void addVehicle();
+    bool isASportsCar(int, bool, int, bool, std::string, bool, bool, bool, bool, int, bool);
+    bool isAPlane(int, bool, int, bool, std::string, std::string, bool, bool, bool, bool, int, bool);
+    bool isABike(int,bool,bool,bool,bool,bool,int,bool);
+    bool isTricycle(int,bool, bool,bool,bool,bool,bool,int,bool);
 
 private:
-
     std::string type;
     std::string name;
     int nWheels;
@@ -32,7 +62,5 @@ private:
     int numWagon;
     bool spareWheel;
     std::string registration;
-
 };
-
-#endif // NEWVEHICLE_H
+#endif // VEHICLE_H
