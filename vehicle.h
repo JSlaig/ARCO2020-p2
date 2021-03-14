292df5817176fa8d923965d096a448b9c3a94897
jsalig00 <36676028+Jsalig00@users.noreply.github.com>
2021-03-14 15:44:59 +0100

    Update vehicle.h

--- vehicle.h
+++ vehicle.h
@@ -1,14 +1,12 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 #include "iostream"
-
-
+#include <string>
 class Vehicle
 {
 public:
-
-    Vehicle(int num);
-    Vehicle(std::string type,std::string name,int nWheels,bool engine,int engineCV,bool fuel,char fuelType,std::string color,bool wings,bool reactor,bool undercarriage,bool locomotive,bool wagon,int numWagon,bool spareWheel,std::string registration);
+    Vehicle();
+    Vehicle(std::string name,int nWheels,bool engine,int engineCV,bool fuel,char fuelType,std::string color,bool wings,bool reactor,bool undercarriage,bool locomotive,bool wagon,int numWagon,bool spareWheel,std::string registration);
 
     /*vehicle::name = name;
     vehicle::nWheels = nWheels;
@@ -23,28 +21,37 @@ public:
     vehicle::locomotive = locomotive;
     vehicle::wagon = wagon;
     vehicle:: spareWheel = spareWheel;
-    vehicle::registration = registration;*/
+    vehicle::registration = registration;*/    
 
+    //Metodos Getter
+    std::string getName();
+    std::string getType();
+    std::string getNWheels();
+    std::string getEngine();
+    std::string getEngineCV();
+    std::string getFuel();
+    std::string getFuelType();
+    std::string getColor();
+    bool getWings();
+    bool getReactor();
+    std::string getUndercarriage();
+    std::string getLocomotive();
+    std::string getWagon();
+    std::string getSpareWheel();
+    std::string getRegistration();    
+
+    bool isASportsCar(int, bool, int, bool, std::string, bool, bool, bool, bool, int, bool);
+    bool isAPlane(int, bool, int, bool, std::string, std::string, bool, bool, bool, bool, int, bool);
+    bool isABike(int,bool,bool,bool,bool,bool,int,bool);
+    bool isTricycle(int,bool, bool,bool,bool,bool,bool,int,bool);
 
 private:
-
     std::string type;
     std::string name;
     int nWheels;
-    bool engine;
-    int engineCV;
-    bool fuel;
-    char fuelType;
-    std::string color;
-    bool wings;
-    bool reactor;
-    bool undercarriage;
-    bool locomotive;
-    bool wagon;
+	@@ -32,7 +62,5 @@ class newVehicle
     int numWagon;
     bool spareWheel;
     std::string registration;
-
 };
-
 #endif // VEHICLE_H
